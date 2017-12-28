99d40537870ade3f310e129a1e58ff1fee83ede2
Adam Hynson <hynsondevelops@gmail.com>
2017-12-28 16:09:22 -0500

    Updated project thumb partial

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,6 +7,22 @@ class ContactsController < ApplicationController
 		@contact = Contact.create(contact_params)
 	end
 
+	def show
+		if (user_signed_in? == true && current_user.name == "Adam Hynson")
+			@contact = Contact.find(params[:id])
+		else	
+			redirect_to portfolio_path
+		end
+	end
+
+	def index
+		if (user_signed_in? == true && current_user.name == "Adam Hynson")
+			@contacts = Contact.all
+		else	
+			redirect_to portfolio_path
+		end
+	end
+
 	private
 
 		def contact_params
