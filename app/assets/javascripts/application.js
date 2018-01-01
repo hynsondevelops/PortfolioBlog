97aa79f35d6e813186cb6bbbee5331945956de29
Adam Hynson <hynsondevelops@gmail.com>
2018-01-01 16:01:58 -0500

    Updating for S3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,6 +21,26 @@
 
 function createImage()
 {
+	$(function() {
+	  $('.directUpload').find("input:file").each(function(i, elem) {
+	    var fileInput    = $(elem);
+	    var form         = $(fileInput.parents('form:first'));
+	    var submitButton = form.find('input[type="submit"]');
+	    var progressBar  = $("<div class='bar'></div>");
+	    var barContainer = $("<div class='progress'></div>").append(progressBar);
+	    fileInput.after(barContainer);
+	    fileInput.fileupload({
+	      fileInput:       fileInput,
+	      url:             form.data('url'),
+	      type:            'POST',
+	      autoUpload:       true,
+	      formData:         form.data('form-data'),
+	      paramName:        'file', // S3 does not like nested name fields i.e. name="user[avatar_url]"
+	      dataType:         'XML',  // S3 returns XML if success_action_status is set to 201
+	      replaceFileInput: false
+	    });
+	  });
+	});
 	var plain_text_box = document.getElementById("content");
 	var plain_text_box_content = plain_text_box.value;
 	var image_name = document.getElementById("img").value.split(/(\\|\/)/g).pop();
