bf1a264a4db50166e799913b78f1cdb690c55f79
Adam Hynson <hynsondevelops@gmail.com>
2018-05-01 12:26:15 -0400

    Updated to Rails 5.1.6 for React

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :post_images
+  resources :project_images
+      # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :users, only: [:edit, :update]
+  resources :posts, param: :title
+  resources :images, only: [:create]
+  resources :tags, param: :name, only: [:show, :create, :index]
+  resources :projects, only: [:new, :create, :show, :index]
+  resources :contacts, only: [:create, :show, :index]
+  get 'markdown_url', to: "images#markdownURL"
+  get 'portfolio', to: "projects#portfolio"
+  get 'markdown', to: "posts#markdown_helper"
+  root "posts#index"
+  get 'users/posts', to: "users#posts"
 end
