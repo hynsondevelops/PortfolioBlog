5eb9117daec8931814b6b7174212624aa5c4a5c3
Adam Hynson <hynsondevelops@gmail.com>
2019-03-25 15:59:22 -0400

    Updated portfolio page.

--- db/schema.rb
+++ db/schema.rb
@@ -15,99 +15,99 @@ ActiveRecord::Schema.define(version: 20180426001803) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "contacts", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+  create_table "contacts", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "images", force: :cascade do |t|
-    t.string   "img_file_name"
-    t.string   "img_content_type"
-    t.integer  "img_file_size"
+  create_table "images", id: :serial, force: :cascade do |t|
+    t.string "img_file_name"
+    t.string "img_content_type"
+    t.integer "img_file_size"
     t.datetime "img_updated_at"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.integer  "project_id"
-    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "project_id"
+    t.integer "post_id"
   end
 
-  create_table "post_images", force: :cascade do |t|
-    t.integer  "post_id"
-    t.string   "image"
+  create_table "post_images", id: :serial, force: :cascade do |t|
+    t.integer "post_id"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.text     "content"
-    t.string   "title"
-    t.integer  "author_id"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.string   "img_file_name"
-    t.string   "img_content_type"
-    t.integer  "img_file_size"
+  create_table "posts", id: :serial, force: :cascade do |t|
+    t.text "content"
+    t.string "title"
+    t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "img_file_name"
+    t.string "img_content_type"
+    t.integer "img_file_size"
     t.datetime "img_updated_at"
-    t.boolean  "draft"
+    t.boolean "draft"
   end
 
-  create_table "project_images", force: :cascade do |t|
-    t.integer  "project_id"
-    t.string   "image"
+  create_table "project_images", id: :serial, force: :cascade do |t|
+    t.integer "project_id"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "projects", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.boolean  "personal_or_work"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.string   "github"
-    t.string   "live_link"
-    t.integer  "author_id"
-    t.string   "image"
+  create_table "projects", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.boolean "personal_or_work"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "github"
+    t.string "live_link"
+    t.integer "author_id"
+    t.string "image"
   end
 
-  create_table "tags", force: :cascade do |t|
-    t.string   "name"
+  create_table "tags", id: :serial, force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "tags_to_posts", force: :cascade do |t|
-    t.integer  "post_id"
-    t.integer  "tag_id"
+  create_table "tags_to_posts", id: :serial, force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.string   "github"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "github"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.string   "img_file_name"
-    t.string   "img_content_type"
-    t.integer  "img_file_size"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.string "img_file_name"
+    t.string "img_content_type"
+    t.integer "img_file_size"
     t.datetime "img_updated_at"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
